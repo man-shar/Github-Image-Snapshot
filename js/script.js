0832eb0f96394b9c881debc6a96ad38beadbb3f0
priyank1205 <priyank.agarwal1205@gmail.com>
2017-11-21 09:57:19 +0530

    Add a preview image

--- js/script.js
+++ js/script.js
@@ -10,14 +10,25 @@ function addHandler(str) {
       return;
 
     var fileName = el.getElementsByClassName("js-navigation-open")[0];
+    var href = "https://raw.githubusercontent.com/" + fileName.href.slice(19);
+    href = href.replace("/blob/","/");
     fileName = fileName.innerText;
     var extension = fileName.slice(fileName.lastIndexOf(".") + 1);
+
+    var injectedHTML = "<img alt='preview' width='15' src='" + href + "'>";
 
     if(imageExtensions.indexOf(extension) >= 0)
     {
       var svg = el.getElementsByTagName("svg")[0];
       svg.parentNode.removeChild(svg);
-    }    
+
+      var icon = el.getElementsByClassName("icon")[0];
+      var image = document.createElement("img");
+      image.setAttribute("alt", "preview");
+      image.setAttribute("width", 20);
+      image.setAttribute("src", href);
+      icon.append(image);
+    }
   });
 };
 
